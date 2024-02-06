739774fa193ed5fc12aa907929818f075c829e74
syoh98 <cjdxla1004@gmail.com>
2024-02-07 01:13:05 +0900

    project setting

---
+++ src/main/java/flab/gotable/GoTableApplication.java
@@ -0,0 +1,17 @@
+package flab.gotable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+@SpringBootApplication
+public class GoTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoTableApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/flab/gotable/GoTableApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.gotable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
