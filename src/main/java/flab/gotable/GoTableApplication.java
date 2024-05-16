af7fcea0af6f9d77929d3ebb83b00df3f01aab78
syoh98 <qorxla1004@gmail.com>
2024-05-17 07:01:59 +0900

    redis 적용

--- src/main/java/flab/gotable/GoTableApplication.java
+++ src/main/java/flab/gotable/GoTableApplication.java
@@ -2,11 +2,10 @@ package flab.gotable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-import java.util.*;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class GoTableApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoTableApplication.class, args);
