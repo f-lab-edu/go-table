c5406dec739fe16dd122ec98c35ec0f2dbbf43d0
syoh98 <qorxla1004@gmail.com>
2024-07-10 21:33:22 +0900

    Mapper 추가

---
+++ src/main/java/flab/gotable/mapper/StoreMapper.java
@@ -0,0 +1,15 @@
+package flab.gotable.mapper;
+
+import flab.gotable.domain.entity.DailySchedule;
+import flab.gotable.domain.entity.SpecificSchedule;
+import flab.gotable.domain.entity.Store;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface StoreMapper {
+    public Store findStoreById(Long id);
+    public List<DailySchedule> findDailyScheduleByStoreId(Long id);
+    public List<SpecificSchedule> findSpecificScheduleByStoreId(Long id);
+}
