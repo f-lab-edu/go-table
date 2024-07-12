1c9ac37bb4a3ea6780ec9f841252c7f19d15b971
syoh98 <qorxla1004@gmail.com>
2024-07-12 22:22:39 +0900

    ResponseEntity 제거

--- src/main/java/flab/gotable/controller/StoreAPIController.java
+++ src/main/java/flab/gotable/controller/StoreAPIController.java
@@ -1,19 +1,16 @@
 package flab.gotable.controller;
 
 import flab.gotable.dto.ApiResponse;
+import flab.gotable.dto.response.StoreDetailsResponseDto;
 import flab.gotable.exception.ErrorCode;
 import flab.gotable.exception.StoreNotFoundException;
 import flab.gotable.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,15 +21,13 @@ public class StoreAPIController {
     private final StoreService storeService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getStoreDetail(@PathVariable("id") Long id) {
-        if(!storeService.getStoreById(id)) {
+    public ApiResponse<StoreDetailsResponseDto> getStoreDetail(@PathVariable("id") Long id) {
+        if(!storeService.existById(id)) {
             throw new StoreNotFoundException(ErrorCode.STORE_NOT_FOUND, ErrorCode.STORE_NOT_FOUND.getMessage());
         }
 
-        Map<String, Object> storeDetails = storeService.getStoreDetail(id);
+        StoreDetailsResponseDto storeDetails = storeService.getStoreDetail(id);
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.ok(storeDetails, "식당 상세 조회 성공"));
-
+        return ApiResponse.ok(storeDetails, "식당 상세 조회 성공");
     }
 }
