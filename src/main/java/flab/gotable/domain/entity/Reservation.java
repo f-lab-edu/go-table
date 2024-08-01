a0fd018d6c9e1e6fbdf83b35765e91d42d804224
syoh98 <qorxla1004@gmail.com>
2024-08-01 15:58:39 +0900

    entity 생성

---
+++ src/main/java/flab/gotable/domain/entity/Reservation.java
@@ -0,0 +1,21 @@
+package flab.gotable.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Reservation {
+    private Long id;
+    private Long seq;
+    private Long restaurantId;
+    private String status;
+    private Long memberCount;
+    private Timestamp createdAt;
+    private Timestamp reservationAt;
+    private String cancelReason;
+}
